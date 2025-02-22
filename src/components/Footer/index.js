c81660340a4e400351432dc2108494484bca0b6a
TreasureUzoma <treasureuzoma650@gmail.com>
2025-02-22 10:25:27 +0100

    feat: customizing to mine

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { useForm } from "react-hook-form";
-import { DribbbleIcon, GithubIcon, LinkedinIcon, TwitterIcon } from "../Icons";
+import { GithubIcon, LinkedinIcon, TwitterIcon } from "../Icons";
 import Link from "next/link";
 import siteMetadata from "@/src/utils/siteMetaData";
 
@@ -68,31 +68,16 @@ const Footer = () => {
         >
           <GithubIcon className="fill-light dark:fill-dark  hover:scale-125 transition-all ease duration-200" />
         </a>
-        <a
-          href={siteMetadata.dribbble}
-          className="inline-block w-6 h-6 mr-4"
-          aria-label="Check my profile on Dribbble"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <DribbbleIcon className="hover:scale-125 transition-all ease duration-200" />
-        </a>
       </div>
 
       <div className="w-full  mt-16 md:mt-24 relative font-medium border-t border-solid border-light py-6 px-8 flex  flex-col md:flex-row items-center justify-between">
         <span className="text-center">
-          &copy;2023 CodeBucks. All rights reserved.
+          &copy;{new Date().getFullYear()} Treasure Uzoma. All rights reserved.
         </span>
-        <Link
-          href="/sitemap.xml"
-          className="text-center underline my-4 md:my-0"
-        >
-          sitemap.xml
-        </Link>
         <div className="text-center">
-          Made with &hearts; by{" "}
-          <a href="https://devdreaming.com" className="underline" target="_blank">
-            CodeBucks
+          Made with ❤️ by{" "}
+          <a href="treasureuzoma.netlify.app" className="underline" target="_blank">
+            Treasure Uzoma
           </a>
         </div>
       </div>
--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,20 +1,20 @@
 // please update required information
 const siteMetadata = {
-    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
-    author: 'CodeBucks',
-    headerTitle: 'Next.js Blog',
+    title: 'Treasures Blog || Treasure Uzoma',
+    author: 'Treasure Uzoma',
+    headerTitle: 'Treasure’s Blog',
     description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
     language: 'en-us',
     theme: 'system', // system, dark or light
     siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social-banner.png', // add social banner in the public folder
-    email: 'codebucks27@gmail.com', 
-    github: 'https://github.com/codebucks27',
-    twitter: 'https://twitter.com/code_bucks',
+    email: 'treasureuzoma650@gmail.com', 
+    github: 'https://github.com/TreasureUzoma',
+    twitter: 'https://twitter.com/TreasureUzoma1',
     facebook: 'https://facebook.com',
-    youtube: 'https://youtube.com/codebucks',
-    linkedin: 'https://www.linkedin.com/in/codebucks/',
+    youtube: 'https://youtube.com/idolodev',
+    linkedin: 'https://www.linkedin.com/in/treasure-uzoma-40a789260',
     dribbble: 'https://www.dribbble.com',
     locale: 'en-US',
   }

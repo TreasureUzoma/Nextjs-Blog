b3221cce15d15cd2181bb423a5e82f1c1c9dd920
TreasureUzoma <treasureuzoma650@gmail.com>
2025-02-23 14:13:45 +0100

    tests

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     assetPrefix: "/blog-static",
+    basePath: "/blog",
     
     compiler: {
         removeConsole: process.env.NODE_ENV === "production" ? true : false

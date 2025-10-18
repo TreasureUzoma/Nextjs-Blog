f5dfd5d5438ac7f85e154a699dcf7c8d502e9d5f
Treasure Uzoma <treasureuzoma650@gmail.com>
2025-10-18 14:45:39 +0100

    feat: change portfolio domain to idolo.dev

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -76,7 +76,7 @@ const Footer = () => {
         </span>
         <div className="text-center">
           Made with ❤️ by{" "}
-          <a href="https://treasureuzoma.netlify.app" className="underline" target="_blank">
+          <a href="https://idolo.dev" className="underline" target="_blank">
             Treasure Uzoma
           </a>
         </div>
--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,22 +1,22 @@
 // please update required information
 const siteMetadata = {
-    title: 'Treasures Blog || Treasure Uzoma',
-    author: 'Treasure Uzoma',
-    headerTitle: 'Treasure’s Blog',
-    description: 'Stay updated with Treasure Uzoma’s Blog.',
-    language: 'en-us',
-    theme: 'system', // system, dark or light
-    siteUrl: 'https://treasureuzoma.netlify.app/blog', // your website URL
-    siteLogo: '/logo.png',
-    socialBanner: '/social-banner.png', // add social banner in the public folder
-    email: 'treasureuzoma650@gmail.com', 
-    github: 'https://github.com/TreasureUzoma',
-    twitter: 'https://twitter.com/idolodev',
-    facebook: 'https://facebook.com',
-    youtube: 'https://youtube.com/idolodev',
-    linkedin: 'https://www.linkedin.com/in/treasure-uzoma-40a789260',
-    dribbble: 'https://www.dribbble.com',
-    locale: 'en-US',
-  }
-  
-  module.exports = siteMetadata
+  title: "Treasures Blog || Treasure Uzoma",
+  author: "Treasure Uzoma",
+  headerTitle: "Treasure’s Blog",
+  description: "Stay updated with Treasure Uzoma’s Blog.",
+  language: "en-us",
+  theme: "system", // system, dark or light
+  siteUrl: "https://idolo.dev/blog", // your website URL
+  siteLogo: "/logo.png",
+  socialBanner: "/social-banner.png", // add social banner in the public folder
+  email: "treasureuzoma650@gmail.com",
+  github: "https://github.com/TreasureUzoma",
+  twitter: "https://twitter.com/idolodev",
+  facebook: "https://facebook.com",
+  youtube: "https://youtube.com/idolodev",
+  linkedin: "https://www.linkedin.com/in/treasure-uzoma-40a789260",
+  dribbble: "https://www.dribbble.com",
+  locale: "en-US",
+};
+
+module.exports = siteMetadata;

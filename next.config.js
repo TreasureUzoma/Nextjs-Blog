05d8b8894b0a84778636676d659d64c123e7e6b5
Treasure Uzoma <treasureuzoma650@gmail.com>
2025-12-11 19:26:38 +0100

    feat: allow server action from main proxies

--- next.config.js
+++ next.config.js
@@ -1,30 +1,37 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    assetPrefix: "/blog-static",
-    
-    compiler: {
-        removeConsole: process.env.NODE_ENV === "production" ? true : false
-    },
+  assetPrefix: "/blog-static",
 
-    webpack: config => {
-        config.plugins.push(new VeliteWebpackPlugin());
-        return config;
-    }
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "blog.idolo.dev",
+        "idolo.dev",
+        "idolodevblog.vercel.app",
+      ],
+    },
+  },
+
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production" ? true : false,
+  },
+
+  webpack: (config) => {
+    config.plugins.push(new VeliteWebpackPlugin());
+    return config;
+  },
 };
 
 class VeliteWebpackPlugin {
-    static started = false;
+  static started = false;
 
-    apply(/** @type {import('webpack').Compiler} */ compiler) {
-        compiler.hooks.beforeCompile.tapPromise(
-            "VeliteWebpackPlugin",
-            async () => {
-                if (VeliteWebpackPlugin.started) return;
-                VeliteWebpackPlugin.started = true;
-                const dev = compiler.options.mode === "development";
-                const { build } = await import("velite");
-                await build({ watch: dev, clean: !dev });
-            }
-        );
-    }
+  apply(/** @type {import('webpack').Compiler} */ compiler) {
+    compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
+      if (VeliteWebpackPlugin.started) return;
+      VeliteWebpackPlugin.started = true;
+      const dev = compiler.options.mode === "development";
+      const { build } = await import("velite");
+      await build({ watch: dev, clean: !dev });
+    });
+  }
 }
